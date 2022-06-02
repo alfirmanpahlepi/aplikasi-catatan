c21c3688f49fdb4ffd3a698956b6a0dc55fb6fe6
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-02 11:32:57 +0700

    fix: remove id

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -11,7 +11,7 @@ export default function NotesList({ title }) {
   };
 
   return (
-    <section id="archived-note">
+    <section>
       <h2>{title}</h2>
       <div className="notes-list">
         {filteredByStatus().length ? (
