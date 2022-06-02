a5a0fc822755e7f62d65ce1c8771f57a01882eac
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-06-02 11:21:36 +0700

    fix: remove state char left

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,19 +1,20 @@
 import useNoteInput from "../hooks/useNoteInput";
 
 export default function NoteInput() {
-  const { setInput, submit, changeTitle, charLeft, input } = useNoteInput();
+  const { submit, changeInput, input } = useNoteInput();
   return (
     <div className="note-input">
       <h2>Buat catatan</h2>
       <form onSubmit={(e) => submit(e)}>
         <p className="note-input__title__char-limit">
-          Sisa karakter: {charLeft}
+          Sisa karakter: {50 - input.title.length}
         </p>
         <input
           type="text"
           name="title"
+          maxLength={50}
           value={input.title}
-          onChange={(e) => changeTitle(e)}
+          onChange={(e) => changeInput(e)}
           className="note-input__title"
           placeholder="Ini adalah judul ..."
           required
@@ -22,7 +23,7 @@ export default function NoteInput() {
           type="text"
           name="body"
           value={input.body}
-          onChange={(e) => setInput({ ...input, body: e.target.value })}
+          onChange={(e) => changeInput(e)}
           className="note-input__body"
           placeholder="Tuliskan catatanmu di sini ..."
           required
